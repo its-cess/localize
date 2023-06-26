781452e8cff64715b90c93be01cb8332ba874da5
Cecily Toro <torocecily@gmail.com>
2023-06-26 13:33:52 -0400

    add merchant store. add merchant login and logout functionality. add user and merchant dashboard pages.

--- src/routes/(dashboard)/merchant-dash/+page.svelte
+++ src/routes/(dashboard)/merchant-dash/+page.svelte
@@ -1,1 +1,8 @@
+<script>
+	import { merchant } from '$lib/stores/MerchantStore';
+
+	let merchantName = $merchant.companyName;
+</script>
+
 <h1>Merchant Dashboard</h1>
+<h2>Welcome back, {merchantName}</h2>
